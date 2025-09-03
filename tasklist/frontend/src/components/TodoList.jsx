44ef86e55a29eaeca4ee217c500f5afc7399714a
Smita Parida <smitaparida182005@gmail.com>
2025-09-04 01:54:43 +0530

    update url

--- tasklist/frontend/src/components/TodoList.jsx
+++ tasklist/frontend/src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import './TodoList.css';
 
 function TodoList({ title, tasks, setTasks }) {
     const [newTask, setNewTask] = useState('');
-    const backendUrl = process.env.REACT_APP_BACKEND_URL;
+    const backendUrl = "https://nora-1qpm.onrender.com/api/tasks";
     const listType = title.toLowerCase();
     const filteredTasks = tasks.filter(t => t.listType === listType);
 
